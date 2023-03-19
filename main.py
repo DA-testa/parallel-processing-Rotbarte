38610a459066ecf59cf96ed07a92c7c747dc2925
Katerina Rotbarte 221RDB292 <125587202+Rotbarte@users.noreply.github.com>
2023-03-19 23:02:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,22 @@
 
 def parallel_processing(n, m, data):
     output = []
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
-
+    threads=[(0,i) for i in range(n)]
+    
+    for i in range(m):
+       thread,start=min(threads,key=lambda x:x[1]) 
+       time=data[i]
+        output.append((thread,start+time))
+        assigned_jobs[thread]=(thread,start+data[i])
     return output
 
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
-
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
-
-    # TODO: create the function
+    n,m = map(int, input().split())
+    data=list(map(int, input().split()))
     result = parallel_processing(n,m,data)
     
-    # TODO: print out the results, each pair in it's own line
+    for thread,start in result:
+        print(thread,start)
 
 
 
